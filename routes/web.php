99c7460882dfd9f3227f5420c85f8b6bfb421c5a
Omar Tawba <omartawba1@gmail.com>
2016-10-20 13:37:35 +0300

    update HomeController & we routes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::group(['middleware' => ['auth']], function () {
+
+    Route::get('/', 'HomeController@index');
+
+});
